34d0b76f881d2b8acb02277854f21d624a10ef80
AJ-01 <AJ-01@AJ-01-PC>
2018-08-10 13:53:57 +0900

    inout

---
+++ AlgorithmJobs/src/extracourse02/InOut.java
@@ -0,0 +1,56 @@
+package extracourse02;
+
+import java.util.Scanner;
+
+public class InOut {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[][] arr = new int[10][10];
+
+		inOut(arr, n);
+
+		sc.close();
+	}
+
+	public static void inOut(int[][] arr, int n) {
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < 10; j++) {
+
+				if (arr[i][j] == 0) {
+
+					arr[i][j] = 1;
+				} else {
+
+					arr[i][j] = 0;
+				}
+				
+				if(i != j) {
+					
+					if (arr[j][i] == 0) {
+
+						arr[j][i] = 1;
+					} else {
+
+						arr[j][i] = 0;
+					}
+				}
+			}
+		}
+		
+		for(int i=0; i<10; i++) {
+			
+			for(int j=0; j<10; j++) {
+				
+				System.out.print(arr[i][j] + " ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
